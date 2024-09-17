5f34d953a981f53b08bd1d53ebd3a6bc3e0d2ae7
Katya1985 <berkat85@rambler.ru>
2024-09-17 13:46:18 +0300

    Add files via upload

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,22 +1,3 @@
-# n = int (input ("Введите число от 3 до 20"))
-# resulte = " "
-# for i in range(3, n + 1):
-#     for j in range(3, n + 1):
-#         if (i %  (i + j) ==0):
-#             resulte += f ("{i}, {j}")
-# print(resulte)
-
-
-
-
-
-
-
-
-
-
-
-
 n = int (input ("Введите число от 3 до 20"))
 resulte = ""
 for i in range (1, n):
@@ -37,21 +18,6 @@ print(resulte)
 
 
 
-n = int (input ("Введите число от 3 до 20"))
-resulte = " "
-import random
-a = [random.randint (3, 21)]
-def pair (a):
-    random = list()
-    b = (a + 1) // 2
-    for i in range(1, b):
-        j = i + 1
-        while i + j <= a:
-            if a % (i + j) == 0:
-                numbers = (i, j)
-                random.append(numbers)
-            j += 1
-    return random
 
 
 
